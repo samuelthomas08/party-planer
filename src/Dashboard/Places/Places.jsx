0c67f5e3cbc6249396e877c541123c454173c6f3
juniorcoder2008 <thomas08samuel@gmail.com>
2025-06-08 21:14:07 +0200

    feat: Ordner umbenennen

--- src/Dashboard/Places/Places.jsx
+++ src/Dashboard/Places/Places.jsx
@@ -1,119 +1,168 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Places.sass';
-import { faPlus, faFolderPlus } from '@fortawesome/free-solid-svg-icons';
+import {
+    faPlus, faFolderPlus, faTrashAlt, faEdit,
+    faArrowsRotate
+} from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useRef, useState } from 'react';
 import Place from './Place/Place';
 import { Link } from 'react-router-dom';
-import { collection, doc, getDocs, query, setDoc } from 'firebase/firestore';
+import {
+    collection, doc, getDoc, onSnapshot, setDoc
+} from 'firebase/firestore';
 import { db } from '../../firebase/firebase';
 import { uid } from 'uid';
 
 const Places = () => {
-
     const [placeList, setPlaceList] = useState([]);
     const [placeFolders, setPlaceFolders] = useState([]);
     const [newFolderModalStatus, setNewFolderModalStatus] = useState(false);
+    const [renameFolderModalStatus, setRenameFolderModalStatus] = useState(false);
 
     const newFolderNameRef = useRef('');
+    const renameFolderNameRef = useRef('');
+    const [renamingFolderId, setRenamingFolderId] = useState('');
 
+    // Firestore Live-Updates für Orte & Ordner
     useEffect(() => {
-        const fetchData = async () => {
-            // Fetch all stored places
-            const placeListQuery = query(collection(db, "places"));
-            const placeListQuerySnapshot = await getDocs(placeListQuery);
-
-            const placeListData = placeListQuerySnapshot.docs.map(el => ({ id: el.id, ...el.data() }));
+        const unsubscribePlaces = onSnapshot(collection(db, "places"), (snapshot) => {
+            const placeListData = snapshot.docs.map(el => ({ id: el.id, ...el.data() }));
             setPlaceList(placeListData);
+        });
 
-            // Fetch all stored folders for places
-            const placeFoldersQuery = query(collection(db, "place-folders"));
-            const placeFoldersQuerySnapshot = await getDocs(placeFoldersQuery);
-
-            const placeFoldersData = placeFoldersQuerySnapshot.docs.map(el => ({ id: el.id, ...el.data() }));
+        const unsubscribeFolders = onSnapshot(collection(db, "place-folders"), (snapshot) => {
+            const placeFoldersData = snapshot.docs.map(el => ({ id: el.id, ...el.data() }));
             setPlaceFolders(placeFoldersData);
-        }
+        });
 
-        fetchData();
+        renameFolderNameRef.current.focus();
+
+        return () => {
+            unsubscribePlaces();
+            unsubscribeFolders();
+        };
     }, []);
 
     const showNewFolderModal = () => {
-        setNewFolderModalStatus(true)
+        setNewFolderModalStatus(true);
     }
-    
+
+    const showRenameFolderModal = (name, id) => {
+        setRenameFolderModalStatus(true);
+        setRenamingFolderId(id);
+
+        if (renameFolderNameRef.current) {
+            renameFolderNameRef.current.value = name;
+            
+        }
+    }
+
     const createNewFolder = (e) => {
         e.preventDefault();
 
-        let folderId = newFolderNameRef.current.value.toLowerCase();
-        folderId = folderId.replace('ä', 'ae');
-        folderId = folderId.replace('ö', 'oe');
-        folderId = folderId.replace('ü', 'ue');
-        folderId = folderId.replace(' ', '_');
-        folderId = folderId + uid(12);
+        let folderId = newFolderNameRef.current.value.toLowerCase()
+            .replace('ä', 'ae')
+            .replace('ö', 'oe')
+            .replace('ü', 'ue')
+            .replace(' ', '_') + uid(12);
 
         const folderData = {
             id: folderId,
             name: newFolderNameRef.current.value,
             places: []
         }
-        
-        setDoc(doc(db, 'place-folders', folderId), folderData);
 
+        setDoc(doc(db, 'place-folders', folderId), folderData);
 
         newFolderNameRef.current.value = '';
         setNewFolderModalStatus(false);
     }
 
+    const renameFolder = async (e) => {
+        e.preventDefault();
+
+        const folderRef = doc(db, 'place-folders', renamingFolderId);
+        const folderSnap = await getDoc(folderRef);
+
+        if (folderSnap.exists()) {
+            const folderData = {
+                id: renamingFolderId,
+                name: renameFolderNameRef.current.value,
+                places: folderSnap.data().places
+            }
+
+            setDoc(folderRef, folderData);
+            setRenameFolderModalStatus(false);
+        }
+    }
+
     return (
         <div className="Places">
-
             <div>
-                
                 <div className="places-section-heading">
                     <h1>Party-Orte</h1>
                     <FontAwesomeIcon className="new-folder" icon={faFolderPlus} onClick={showNewFolderModal} />
                 </div>
-                {newFolderModalStatus ? <div className="new-folder-modal">
+
+                <div className={newFolderModalStatus ? "new-folder-modal" : "new-folder-modal-invisible"}>
                     <form>
                         <p>Name des Ordners</p>
                         <div>
-                            <input autoFocus type="text" ref={newFolderNameRef} />   
-                            <button type="submit" className="new-folder-btn" onClick={e => createNewFolder(e)}>
+                            <input autoFocus type="text" ref={newFolderNameRef} />
+                            <button type="submit" className="new-folder-btn" onClick={createNewFolder}>
                                 <FontAwesomeIcon icon={faPlus} className='new-folder-btn-icon' />
                             </button>
                         </div>
-
                     </form>
-                </div> : null}
-                
+                </div>
+
+                <div className={renameFolderModalStatus ? "rename-folder-modal" : "rename-folder-modal-invisible"}>
+                    <form>
+                        <p>Ordner umbenennen</p>
+                        <div>
+                            <input autoFocus type="text" ref={renameFolderNameRef} />
+                            <button type="submit" className="rename-folder-btn" onClick={renameFolder}>
+                                <FontAwesomeIcon icon={faArrowsRotate} className='rename-folder-btn-icon' />
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
                 <div className="places">
                     {placeList.length === 0 ? (
                         <p>Du hast noch keine Orte eingespeichert...</p>
                     ) : (
                         placeFolders.map((placeFolder, index) => (
                             <div className='folder' key={index}>
-                                <h2 className='folder-heading'>{placeFolder.name}</h2>
-                                {placeFolder.places.map(place => {
-                                    const fetchedPlace = placeList.find(id => id.nameId === place);
+                                <div className='folder-heading-container'>
+                                    <h2 className='folder-heading'>{placeFolder.name}</h2>
+                                    <FontAwesomeIcon className='icon' icon={faEdit} onClick={() => showRenameFolderModal(placeFolder.name, placeFolder.id)} />
+                                    <FontAwesomeIcon className='icon' icon={faTrashAlt} />
+                                </div>
+                                {placeFolder.places.length === 0 ? <p>Hier sind noch keine Orte</p> : placeFolder.places.map(place => {
+                                    const fetchedPlace = placeList.find(p => p.nameId === place);
                                     if (!fetchedPlace) return null;
                                     return (
-                                    <Place
-                                        key={fetchedPlace.nameId}
-                                        placeList={placeList}
-                                        setPlaceList={setPlaceList}
-                                        title={fetchedPlace.name}
-                                        placeId={fetchedPlace.nameId}
-                                    />
+                                        <Place
+                                            key={fetchedPlace.nameId}
+                                            placeList={placeList}
+                                            setPlaceList={setPlaceList}
+                                            title={fetchedPlace.name}
+                                            placeId={fetchedPlace.nameId}
+                                        />
                                     );
                                 })}
                             </div>
                         ))
                     )}
-
-
                 </div>
             </div>
 
-            <Link to='/new-place'><button className="create-new-place-btn"><FontAwesomeIcon icon={faPlus} /> Neuen Ort erstellen</button></Link>
+            <Link to='/new-place'>
+                <button className="create-new-place-btn">
+                    <FontAwesomeIcon icon={faPlus} /> Neuen Ort erstellen
+                </button>
+            </Link>
         </div>
     );
 }
